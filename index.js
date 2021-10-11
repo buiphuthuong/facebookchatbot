e39fdb87dbf304f298877301e8fdac2c7daf215b
ThuongBui <buiphuthuong@gmail.com>
2021-10-11 18:02:53 +0700

    chatbot plugin vv4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 const app = express().use(bodyParser.json())
 
 // Handles messages events
