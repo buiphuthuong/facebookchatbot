2762c28689d026239af592c2d2c1043278fc4ede
ThuongBui <buiphuthuong@gmail.com>
2021-10-11 16:37:55 +0700

    chatbot plugin vv2

--- index.js
+++ index.js
@@ -3,17 +3,70 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express().use(bodyParser.json())
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  let response
+
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response)
+}
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  let response
+
+  // Get the payload for the postback
+  let payload = received_postback.payload
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { text: 'Thanks!' }
+  } else if (payload === 'no') {
+    response = { text: 'Oops, try sending another image.' }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response)
+}
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid
+    },
+    message: response
+  }
+}
 app.post('/webhook', (req, res) => {
   let body = req.body
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
     // Iterates over each entry - there may be multiple if batched
+
     body.entry.forEach(function (entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0]
       console.log(webhook_event)
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id
+      console.log('Sender PSID: ' + sender_psid)
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message)
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback)
+      }
     })
 
     // Returns a '200 OK' response to all requests
