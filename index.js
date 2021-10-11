fa2ce0e9225629196c5f2c96c4c8eef839445a93
ThuongBui <buiphuthuong@gmail.com>
2021-10-12 00:33:23 +0700

    chatbot plugin vv4

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ const callSendAPI = async (sender_psid, response) => {
   }
 
   // Send the HTTP request to the Messenger Platform
+  console.log(request_body)
   request(
     {
       uri: 'https://graph.facebook.com/v2.6/me/messages',
