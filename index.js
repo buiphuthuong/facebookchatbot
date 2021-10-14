f0d819da4057a17544864321b2c395d5b7a5c4cc
ThuongBui <buiphuthuong@gmail.com>
2021-10-14 22:24:10 +0700

    chatbot plugin vv4

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const callSendAPI = async (sender_psid, response) => {
     recipient: {
       id: sender_psid
     },
-    message: response
+    message: { text: response }
   }
 
   // Send the HTTP request to the Messenger Platform
