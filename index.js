5fc2d724fe24c69e3cf06f6cf5f12515470c8270
ThuongBui <buiphuthuong@gmail.com>
2021-10-18 23:52:52 +0700

    first commit

--- index.js
+++ index.js
@@ -68,19 +68,27 @@ const getProducts = async (cat) => {
     )
     //  console.log(res.data)
     // console.log('aaa')
+
     res.data.forEach((element) => {
       let elementObj = {
-        title: 'Classic T-Shirt Collection',
-        subtitle: 'See all our colors',
-        image_url: 'https://peterssendreceiveapp.ngrok.io/img/collection.png',
+        title: element.title,
+        image_url: element.img,
+        subtitle: 'We have the right hat for everyone.',
+        default_action: {
+          type: 'web_url',
+          url: 'https://petersfancybrownhats.com/view?item=103',
+          webview_height_ratio: 'tall'
+        },
         buttons: [
           {
-            title: 'View',
             type: 'web_url',
-            url: 'https://peterssendreceiveapp.ngrok.io/collection',
-            messenger_extensions: true,
-            webview_height_ratio: 'tall',
-            fallback_url: 'https://peterssendreceiveapp.ngrok.io/'
+            url: 'https://petersfancybrownhats.com',
+            title: 'View Website'
+          },
+          {
+            type: 'postback',
+            title: 'Start Chatting',
+            payload: 'DEVELOPER_DEFINED_PAYLOAD'
           }
         ]
       }
@@ -184,59 +192,14 @@ async function handlePostback(senderPsid, receivedPostback) {
   if (payload === 'shopping') {
     response = responseProductType
   } else if (payload === 'quan-ao-nam') {
-    //  const elementArray = await getProducts('tshirt')
+    const elementArray = await getProducts('tshirt')
     // console.log('elementArray', elementArray)
     const responseProductList = {
       attachment: {
         type: 'template',
         payload: {
           template_type: 'generic',
-          elements: [
-            {
-              title: 'Welcome!',
-              image_url: 'https://petersfancybrownhats.com/company_image.png',
-              subtitle: 'We have the right hat for everyone.',
-              default_action: {
-                type: 'web_url',
-                url: 'https://petersfancybrownhats.com/view?item=103',
-                webview_height_ratio: 'tall'
-              },
-              buttons: [
-                {
-                  type: 'web_url',
-                  url: 'https://petersfancybrownhats.com',
-                  title: 'View Website'
-                },
-                {
-                  type: 'postback',
-                  title: 'Start Chatting',
-                  payload: 'DEVELOPER_DEFINED_PAYLOAD'
-                }
-              ]
-            },
-            {
-              title: 'Welcome!',
-              image_url: 'https://petersfancybrownhats.com/company_image.png',
-              subtitle: 'We have the right hat for everyone.',
-              default_action: {
-                type: 'web_url',
-                url: 'https://petersfancybrownhats.com/view?item=103',
-                webview_height_ratio: 'tall'
-              },
-              buttons: [
-                {
-                  type: 'web_url',
-                  url: 'https://petersfancybrownhats.com',
-                  title: 'View Website'
-                },
-                {
-                  type: 'postback',
-                  title: 'Start Chatting',
-                  payload: 'DEVELOPER_DEFINED_PAYLOAD'
-                }
-              ]
-            }
-          ]
+          elements: elementArray
         }
       }
     }
