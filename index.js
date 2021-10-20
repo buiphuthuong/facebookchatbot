de5ca06d9967380814e4e98af4a39430f4e8c574
ThuongBui <buiphuthuong@gmail.com>
2021-10-20 22:59:10 +0700

    first commit

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ function handleMessage(senderPsid, receivedMessage) {
   let typeMessage = ''
   if (receivedMessage.text) {
     const message = receivedMessage.text.toLowerCase()
-
+    console.log(message)
     if (message.includes('SKU:')) {
       const sku = string.split(':')[1]
       console.log(sku)
@@ -186,7 +186,7 @@ function handleMessage(senderPsid, receivedMessage) {
       typeMessage = proccessMessage(message)
     }
 
-    console.log(typeMessage)
+    console.log('typeMessage', typeMessage)
     switch (typeMessage) {
       case XIN_CHAO:
         response = responseFirstQuestion
