61029557a0395ddf47361ad83d885506c55a443a
ThuongBui <buiphuthuong@gmail.com>
2021-10-22 00:04:20 +0700

    first commit

--- response.js
+++ response.js
@@ -16,35 +16,30 @@ const responseQuickreply = {
   ]
 }
 const responseFirstQuestion = {
-  attachment: {
-    type: 'template',
-    payload: {
-      template_type: 'generic',
-      elements: [
+  persistent_menu: [
+    {
+      locale: 'default',
+      composer_input_disabled: false,
+      call_to_actions: [
         {
-          title: 'Chào bạn! Bạn cần mua hàng hay hỗ trợ ạ!',
-          subtitle: 'Nhấn nút để trả lời ạ!',
-          buttons: [
-            {
-              type: 'postback',
-              title: 'Cần mua hàng',
-              payload: 'shopping'
-            },
-            {
-              type: 'postback',
-              title: 'Tìm sản phẩm',
-              payload: 'check-product'
-            },
-            {
-              type: 'postback',
-              title: 'Hỗ trợ!',
-              payload: 'support'
-            }
-          ]
+          type: 'postback',
+          title: 'Talk to an agent',
+          payload: 'CARE_HELP'
+        },
+        {
+          type: 'postback',
+          title: 'Outfit suggestions',
+          payload: 'CURATION'
+        },
+        {
+          type: 'web_url',
+          title: 'Shop now',
+          url: 'https://www.originalcoastclothing.com/',
+          webview_height_ratio: 'full'
         }
       ]
     }
-  }
+  ]
 }
 const responseClipSKU = {
   attachment: {
